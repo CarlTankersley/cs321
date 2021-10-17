41e60a26d8527bdf05dd9d3a70e1abe458fb7855
Carl Tankersley <tankersleyc@carleton.edu>
2021-10-17 16:26:32 -0500

    IT FINALLY WORKSgit add .!

--- mcts/MCTS.py
+++ mcts/MCTS.py
@@ -13,15 +13,15 @@ you can take a look at game1.py to get a sense of how the game is structured.
     - Node.updateValue()'''
 
 # These imports are used by the starter code.
+
+# You will want to use this import in your code
+
+# Whether to display the UCB rankings at each turn.
 import random
 import argparse
 import game1
-
-# You will want to use this import in your code
 import math
-
-# Whether to display the UCB rankings at each turn.
-DISPLAY_BOARDS = False 
+DISPLAY_BOARDS = False
 
 # UCB_CONST value - you should experiment with different values
 UCB_CONST = .5
@@ -29,18 +29,19 @@ UCB_CONST = .5
 
 class Node(object):
     """Node used in MCTS"""
-    
+
     def __init__(self, state, parent_node):
         """Constructor for a new node representing game state
         state. parent_node is the Node that is the parent of this
         one in the MCTS tree. """
         self.state = state
         self.parent = parent_node
-        self.children = {} # maps moves (keys) to Nodes (values); if you use it differently, you must also change addMove
+        # maps moves (keys) to Nodes (values); if you use it differently, you must also change addMove
+        self.children = {}
         self.visits = 0
         self.value = 0
         # Note: you may add additional fields if needed
-        
+
     def addMove(self, move):
         """
         Adds a new node for the child resulting from move if one doesn't already exist.
@@ -51,7 +52,7 @@ class Node(object):
             self.children[move] = Node(state, self)
             return True
         return False
-    
+
     def getValue(self):
         """
         Gets the value estimate for the current node. Value estimates should correspond
@@ -65,17 +66,15 @@ class Node(object):
         outcome: +1 for 1st player win, -1 for 2nd player win, 0 for draw."""
         "*** YOUR CODE HERE ***"
         wins = self.value * self.visits
-        if outcome == self.state.getTurn():
-            wins += 1
-        elif outcome == 0:
-            wins += 0.5
+        if outcome != self.state.getTurn(): # it was playing perfectly backwards before, so this is an 
+            wins += 1                       # effective, albeit hacky, way to fix that
         self.visits += 1
         self.value = wins / self.visits
 
     def UCBValue(self):
         """Value from the UCB formula used by parent to select a child. """
         if self.visits == 0:
-            return 0
+            return 1
         return self.value + UCB_CONST * math.sqrt(math.log(self.parent.visits) / self.visits)
 
 def MCTS(root, rollouts):
@@ -102,21 +101,23 @@ def MCTS(root, rollouts):
         else:
             outcome = node.state.value()
         backpropagate(outcome, node, root)
-    return bestValue(node)
+    return bestValue(root)
+
 
 def select(node):
     while not node.state.isTerminal() and len(node.children) == len(node.state.getMoves()):
         node = bestUCB(node)
     return node
 
+
 def bestUCB(node):
-    turn = node.state.getTurn()
-    curr_best = (-100 * turn, None)
+    curr_best = (-1, None)
     for key in node.children.keys():
         child = node.children[key]
-        if turn * child.UCBValue() > turn * curr_best[0]:
-            curr_best = (turn * child.UCBValue(), child)
+        if child.UCBValue() > curr_best[0]:
+            curr_best = (child.UCBValue(), child)
     return curr_best[1]
+
 
 def expand(node):
     moves = node.state.getMoves()
@@ -127,10 +128,12 @@ def expand(node):
             break
     return child
 
+
 def random_playout(node):
     while not node.state.isTerminal():
-        node = Node(node.state.nextState(random_move(node)), None)
-    return node.state.value()
+        node = Node(node.state.nextState(random_move(node)), None) # no parent so that it's not connected to the tree
+    return node.state.value()                                      # and gets garbage collected, since it's a temp node
+
 
 def backpropagate(outcome, node, root):
     while True:
@@ -139,13 +142,13 @@ def backpropagate(outcome, node, root):
             break
         node = node.parent
 
+
 def bestValue(node):
-    turn = -node.state.getTurn()
-    curr_best = (float('inf') * turn, None)
+    curr_best = (-1, None)
     for key in node.children.keys():
         child = node.children[key]
-        if child.value * turn > turn * curr_best[0]:
-            curr_best = (child.value * turn, key)
+        if child.value > curr_best[0]:
+            curr_best = (child.value, key)
     return curr_best[1]
 
 
@@ -154,19 +157,19 @@ def parse_args():
     Parse command line arguments.
     """
     p = argparse.ArgumentParser()
-    p.add_argument("-r", "--rollouts", type=int, default=0, help="Number of root-to-leaf "+\
-                    "play-throughs that MCTS should run). Default=0 (random moves)")
-    p.add_argument("-n", "--numGames", type=int, default=0, help="Number of games "+\
-                    "to play). Default=1")
-    p.add_argument("-s", "--second", action="store_true", help="Set this flag to "+\
-                    "make your agent move second.")
-    p.add_argument("-d", "--displayBoard", action="store_true", help="Set this flag to "+\
-                    "make display the board at each MCTS turn with MCTS's rankings of moves.")
-    p.add_argument("-sa", "--rolloutsSecondMCTSAgent", type=int, default=0, help="If non-0, other player "+\
-                    "will also be an MCTS agent and will use the number of rollouts set with this "+\
-                    "argument. Default=0 (other player is random)")
-    p.add_argument("-c", "--ucbConst", type=float, default=.5, help="Value for the UCB exploration "+\
-                    "constant. Default=.5")
+    p.add_argument("-r", "--rollouts", type=int, default=0, help="Number of root-to-leaf " +
+                   "play-throughs that MCTS should run). Default=0 (random moves)")
+    p.add_argument("-n", "--numGames", type=int, default=0, help="Number of games " +
+                   "to play). Default=1")
+    p.add_argument("-s", "--second", action="store_true", help="Set this flag to " +
+                   "make your agent move second.")
+    p.add_argument("-d", "--displayBoard", action="store_true", help="Set this flag to " +
+                   "make display the board at each MCTS turn with MCTS's rankings of moves.")
+    p.add_argument("-sa", "--rolloutsSecondMCTSAgent", type=int, default=0, help="If non-0, other player " +
+                   "will also be an MCTS agent and will use the number of rollouts set with this " +
+                   "argument. Default=0 (other player is random)")
+    p.add_argument("-c", "--ucbConst", type=float, default=.5, help="Value for the UCB exploration " +
+                   "constant. Default=.5")
     args = p.parse_args()
     if args.displayBoard:
         global DISPLAY_BOARDS
@@ -183,6 +186,7 @@ def random_move(node):
     move = random.choice(node.state.getMoves())
     node.addMove(move)
     return move
+
 
 def run_multiple_games(num_games, args):
     """
@@ -205,6 +209,7 @@ def run_multiple_games(num_games, args):
             draws += 1
     print("Player 1 games won: " + str(player1GamesWon) + "/" + str(num_games))
     print("Draws: " + str(draws) + "/" + str(num_games))
+
 
 def play_game(args):
     """
@@ -244,8 +249,6 @@ def play_game(args):
             node2 = node2.children[move]
     return node
 
-            
-    
 
 def main():
     """
@@ -260,7 +263,7 @@ def main():
     else:
         # Play the game
         node = play_game(args)
-    
+
         # Print result
         winner = node.state.value()
         print(game1.print_board(node.state))
@@ -270,7 +273,7 @@ def main():
             print("Player 2 wins")
         else:
             print("It's a draw")
-            
-            
+
+
 if __name__ == "__main__":
     main()
